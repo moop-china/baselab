1ed8201acbc4736173d79bc6fbe3c30757b0195d
shark_wang <longwind.wang@gmail.com>
2019-02-18 11:19:43 +0800

    init binder

---
+++ binder/install.R
@@ -0,0 +1,21 @@
+### install regular packages
+
+install.packages("agricolae") # python support in RMarkdown
+install.packages("ggplot2") # python support in RMarkdown
+install.packages("xlsx") # python support in RMarkdown
+install.packages("data.table") # python support in RMarkdown
+install.packages("lubridate") # python support in RMarkdown
+install.packages("plyr") # python support in RMarkdown
+install.packages("zoo") # python support in RMarkdown
+install.packages("scales") # python support in RMarkdown
+install.packages("C50") # python support in RMarkdown
+install.packages("mice") # python support in RMarkdown
+install.packages("Hmisc") # python support in RMarkdown
+
+### install bioconductor packages
+# install.packages("BiocManager")
+# BiocManager::install("package")
+
+### install GitHub packages (tag = commit, branch or release tag)
+# install.packages("devtools")
+# devtools::install_github("user/repo", ref = "tag")
